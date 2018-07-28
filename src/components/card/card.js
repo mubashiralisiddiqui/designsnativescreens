b1486d0da856071b5eaa59c5025323e89a4b3029
mubashir <mubashir22ali@gmail.com>
2018-07-29 01:42:18 +0500

    changing build version

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -16,7 +16,7 @@ export default class AppHeader extends Component {
                 >
                     <Text style={{ marginBottom: 10 }}>
                         The idea with React Native Elements is more about component structure than actual design.
-  </Text>
+                    </Text>
                     <Button
                         // onPress={() => { navigate('Packagecontainer') }}
                         icon={<Icon name='code' color='#ffffff' />}
