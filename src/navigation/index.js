4c081013c410894aa0e1ff47ac3bef6926c8532c
mubashir <mubashir22ali@gmail.com>
2018-07-29 15:07:42 +0500

    adding mapscreen in navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,14 +1,15 @@
 import { StackNavigator } from 'react-navigation';
-import { SliderContainer, Packagecontainer } from '../container'
+import { SliderContainer, Packagecontainer, Mapscreen } from '../container'
 
 
 
 
 export const AppNav = StackNavigator({
     sliderScreen: { screen: SliderContainer },
-    listScreen: { screen: Packagecontainer }
+    listScreen: { screen: Packagecontainer },
+    Mapscreen: { screen: Mapscreen }
 }, {
         headerMode: 'screen',
-        initialRouteName: 'sliderScreen'
+        initialRouteName: 'Mapscreen'
     }
 )
